e26d14b1942c00354ce4a2cd6880d342220dc392
fakru <fakrudeena@zeta.tech>
2022-01-19 19:00:34 +0530

    created employee datastore
    Singleton EmployeeDataMap class with add, get & getAll

---
+++ src/main/java/com/ganesha/people/datastore/EmployeeData.java
@@ -0,0 +1,36 @@
+package com.ganesha.people.datastore;
+
+import com.ganesha.people.model.Employee;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EmployeeData {
+
+  private static EmployeeData employeeData;
+  private final Map<Integer, Employee> employeeMap = new HashMap<>();
+
+  // TODO : handle synchronisation
+  public static EmployeeData getInstance() {
+    if (employeeData == null) {
+      employeeData = new EmployeeData();
+    }
+    return employeeData;
+  }
+
+  public static Employee addEmployee(int empId, Employee employee) {
+    return getDataMap().put(empId, employee);
+  }
+
+  public static Employee getEmployee(int empId) {
+    return getDataMap().get(empId);
+  }
+
+  public static Map<Integer, Employee> getDataMap() {
+    return getInstance().getEmployeeMap();
+  }
+}
